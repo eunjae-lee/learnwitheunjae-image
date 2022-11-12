e15c9c773a813e40ea2407c778a338e11a3f5f65
Eunjae Lee <hey@eunjae.dev>
2022-11-12 23:22:09 +0100

    .

--- src/components/RootPage.tsx
+++ src/components/RootPage.tsx
@@ -1,17 +1,19 @@
 export default function RootPage() {
   return (
     <div
-      tw="h-full w-full flex flex-col justify-center items-center p-20"
+      tw="h-full w-full flex justify-center items-center p-20"
       style={{ backgroundColor: "#DE4500", color: "#FBF4EF" }}
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        tw="rounded-full w-32"
-        src="og.learnwitheunjae.dev/profile.jpeg"
+        tw="rounded-full w-64"
+        src="https://og.learnwitheunjae.dev/profile.jpeg"
         alt="Eunjae Lee"
       />
-      <p tw="text-7xl font-thin">{"Hello world!!"}</p>
-      <p tw="text-7xl font-black">테스트</p>
+      <div tw="ml-16 mr-16 flex flex-col">
+        <p style={{ fontSize: "50px", lineHeight: 0.6 }}>Learn with</p>
+        <p style={{ fontSize: "80px", lineHeight: 0.6 }}>Eunjae</p>
+      </div>
     </div>
   );
 }
